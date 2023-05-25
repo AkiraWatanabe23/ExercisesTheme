d8c813caff80708a69741b6a7f121ec120172233
Akira Watanabe <a0323w@gmail.com>
2023-05-25 22:16:18 +0900

    [add] FadeSystem

--- Assets/CourseContents/Sample.cs
+++ Assets/CourseContents/Sample.cs
@@ -4,36 +4,26 @@ public class Sample : MonoBehaviour
 {
     private void Start()
     {
-        //リテラル...コード内に埋め込むことができるデータ
-        #region リテラル一覧
-        //Debug.Log(1234); //整数リテラル
-        //Debug.Log(3.14); //浮動小数点数リテラル
-        //Debug.Log(true); //ブーリアンリテラル（true, false）
-        //Debug.Log('a');  //文字リテラル
-        //Debug.Log("Hello World"); //文字列リテラル
-        //Debug.Log(null); //nullリテラル（参照なし）
-        #endregion
-
-        ulong data = 0b_11111111_10000001_10000001_10010001_10000001_10000001_10000001_11111111;
-        GenerateMap(data);
+        //ulong data = 0b_11111111_10000001_10000001_10010001_10000001_10000001_10000001_11111111;
+        //GenerateMap(data);
     }
 
-    private void GenerateMap(ulong num)
-    {
-        //白 or 黒のセルを 8 * 8 に生成
-        for (int i = 7; i > 0; i++)
-        {
-            for (int j = 7; j > 0; j++)
-            {
-                var cell = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cell.transform.position = new(j * 1.1f, i * 1.1f, 0);
+    //private void GenerateMap(ulong num)
+    //{
+    //    //白 or 黒のセルを 8 * 8 に生成
+    //    for (int i = 7; i > 0; i++)
+    //    {
+    //        for (int j = 7; j > 0; j++)
+    //        {
+    //            var cell = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    //            cell.transform.position = new(j * 1.1f, i * 1.1f, 0);
 
-                var renderer = cell.GetComponent<Renderer>();
-                renderer.material.color =
-                    (num & 1) == 0 ? Color.white : Color.black;
+    //            var renderer = cell.GetComponent<Renderer>();
+    //            renderer.material.color =
+    //                (num & 1) == 0 ? Color.white : Color.black;
 
-                num >>= 1;
-            }
-        }
-    }
+    //            num >>= 1;
+    //        }
+    //    }
+    //}
 }
---
+++ Assets/CourseContents/Sample2.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Sample2 : MonoBehaviour
+{
+    [Tooltip("オブジェクト数")]
+    [SerializeField] private int _cellCount = 0;
+
+    private Image[] _images = default;
+    private int _index = 0;
+
+    private void Start()
+    {
+        _images = new Image[_cellCount];
+
+        for (var i = 0; i < _cellCount; i++)
+        {
+            var obj = new GameObject($"Cell{i}");
+            obj.transform.parent = transform;
+
+            var image = obj.AddComponent<Image>();
+            image.color = i == 0 ? Color.red : Color.white;
+
+            _images[i] = obj.GetComponent<Image>();
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) // 左キーを押した
+        {
+            if (_index != 0)
+            {
+                SelectCell(true);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) // 右キーを押した
+        {
+            if (_index != _cellCount - 1)
+            {
+                SelectCell(false);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //オブジェクトを破壊
+            Destroy(_images[_index].gameObject);
+
+            //配列をつめる
+            for (int i = _index; i < _cellCount - 1; i++)
+            {
+                _images[i] = _images[i + 1];
+            }
+            //カウントを減らす
+            _cellCount--;
+
+            SelectCell(false);
+        }
+    }
+
+    private void SelectCell(bool dir)
+    {
+        if (_cellCount == 0) return;
+
+        _images[_index].color = Color.white;
+        _index = dir ? _index - 1 : _index + 1;
+
+        _images[_index].color = Color.red;
+    }
+}
